9a284222eafde8aa5ed44d87f735c5292cd3fb9a
Danilo1dev <danilofoxlog@gmail.com>
2022-07-19 20:37:26 -0300

    teste de class ClientePetAplication

--- src/main/java/br/com/petz/clientepet/ClientePetApplication.java
+++ src/main/java/br/com/petz/clientepet/ClientePetApplication.java
@@ -2,10 +2,18 @@ package br.com.petz.clientepet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class ClientePetApplication {
 
+	public String getNomeTeste() {
+		return "Cliente Pet - API";
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ClientePetApplication.class, args);
 	}
