b803513f5e2185f60778a5e56900471cb0ab8718
Danilo1dev <danilofoxlog@gmail.com>
2022-07-19 20:45:35 -0300

    implementado @Getmapping na class ClientePetAplication

--- src/main/java/br/com/petz/clientepet/ClientePetApplication.java
+++ src/main/java/br/com/petz/clientepet/ClientePetApplication.java
@@ -2,6 +2,7 @@ package br.com.petz.clientepet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,11 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/")
 public class ClientePetApplication {
-
+	@GetMapping
 	public String getNomeTeste() {
 		return "Cliente Pet - API";
 	}
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(ClientePetApplication.class, args);
 	}
