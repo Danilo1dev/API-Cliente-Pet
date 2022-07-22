2872635a638d11582fdf3875330a12aaa6c2e447
Danilo1dev <danilofoxlog@gmail.com>
2022-07-21 21:55:56 -0300

    implementa classe Repository

--- src/main/java/br/com/petz/clientepet/cliente/application/repository/ClienteRepository.java
+++ src/main/java/br/com/petz/clientepet/cliente/application/repository/ClienteRepository.java
@@ -1,4 +1,4 @@
-package br.com.petz.clientepet.cliente.application.service;
+package br.com.petz.clientepet.cliente.application.repository;
 
 import br.com.petz.clientepet.cliente.domain.Cliente;
 
