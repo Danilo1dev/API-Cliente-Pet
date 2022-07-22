224992c5cf57d549b3c865fa7264ae9ff76211b0
Danilo1dev <danilofoxlog@gmail.com>
2022-07-21 21:45:04 -0300

    implementa ClienteRepository e seus metodos

--- src/main/java/br/com/petz/clientepet/cliente/application/service/ClienteApplicationService.java
+++ src/main/java/br/com/petz/clientepet/cliente/application/service/ClienteApplicationService.java
@@ -4,16 +4,23 @@ import org.springframework.stereotype.Service;
 
 import br.com.petz.clientepet.cliente.application.api.ClienteRequest;
 import br.com.petz.clientepet.cliente.application.api.ClienteResponse;
+import br.com.petz.clientepet.cliente.domain.Cliente;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
 @Service
 @Log4j2
+@RequiredArgsConstructor
 public class ClienteApplicationService implements ClienteService {
+	private final ClienteRepository clienteRepository;
 
 	@Override
 	public ClienteResponse criaCliente(ClienteRequest clienteRequest) {
 		log.info("[inicia] ClienteApplicationService - criaCliente");
+		Cliente cliente = clienteRepository.salva(new Cliente(clienteRequest));
 		log.info("[finaliza] ClienteApplicationService - criaCliente");
-		return null;
+		return ClienteResponse.builder()
+				.idCliente(cliente.getIdCliente())
+				.build();
 	}
 }
---
+++ src/main/java/br/com/petz/clientepet/cliente/application/service/ClienteRepository.java
@@ -0,0 +1,8 @@
+package br.com.petz.clientepet.cliente.application.service;
+
+import br.com.petz.clientepet.cliente.domain.Cliente;
+
+public interface ClienteRepository {
+	Cliente salva(Cliente cliente);
+
+}
