a1298f6f3393683da9a851984b51f3511d680150
Danilo1dev <danilofoxlog@gmail.com>
2022-08-03 21:03:47 -0300

    implementa delete

--- src/main/java/br/com/petz/clientepet/cliente/application/service/ClienteApplicationService.java
+++ src/main/java/br/com/petz/clientepet/cliente/application/service/ClienteApplicationService.java
@@ -48,7 +48,7 @@ public class ClienteApplicationService implements ClienteService {
 	public void deletaClienteAtravesID(UUID idCliente) {
 		log.info("[inicia] ClienteApplicationService - deletaClienteAtravesId");
 		Cliente cliente = clienteRepository.buscaClienteAtravesId(idCliente);
-		//clienteRepository.deletaCliente(cliente);
+		clienteRepository.deletaCliente(cliente);
 		log.info("[finaliza] ClienteApplicationService - deletaClienteAtravesId");
 	}
 }
--- src/main/java/br/com/petz/clientepet/cliente/infra/ClienteInfraRepository.java
+++ src/main/java/br/com/petz/clientepet/cliente/infra/ClienteInfraRepository.java
@@ -41,4 +41,11 @@ public class ClienteInfraRepository implements ClienteRepository {
 		log.info("[finaliza]ClienteInfraRepository - buscaClienteAtravesID");
 		return cliente;
 	}
+
+	@Override
+	public void deletaCliente(Cliente cliente) {
+		log.info("[inicia]ClienteInfraRepository - deletaCliente");
+		clienteSpringDataJPARepository.delete(cliente);
+		log.info("[finaliza]ClienteInfraRepository - deletaCliente");	
+	}
 }
