92f6105f29db7f939bf1637850c07f8f56b6265b
Danilo1dev <danilofoxlog@gmail.com>
2022-08-04 19:05:39 -0300

    ajuste de espaço

--- src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteAPI.java
+++ src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteAPI.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/v1/cliente")
 public interface ClienteAPI {
+	
 	@PostMapping
 	@ResponseStatus(code = HttpStatus.CREATED)
 	ClienteResponse postCliente(@Valid @RequestBody ClienteRequest clienteRequest);
