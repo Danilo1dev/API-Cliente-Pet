d25796303ef632b2ba9d13db24cbb7bde488e825
Danilo1dev <danilofoxlog@gmail.com>
2022-09-25 10:59:30 -0300

    implementa controller

--- src/main/java/br/com/petz/clientepet/pet/application/api/PetController.java
+++ src/main/java/br/com/petz/clientepet/pet/application/api/PetController.java
@@ -1,5 +1,6 @@
 package br.com.petz.clientepet.pet.application.api;
 
+import java.util.List;
 import java.util.UUID;
 import javax.validation.Valid;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,5 +21,13 @@ public class PetController implements PetAPI {
 		PetResponse pet = petService.criaPet(idCliente, petRequest);
 		log.info("[finaliza] PetController - postPet");
 		return pet;
+	}
+
+	@Override
+	public List<PetListResponse> getPetDoClienteComId(UUID idCliente) {
+		log.info("[inicia] PetController - getPetDoClienteComId");
+		log.info("[idCliente] {}", idCliente);
+		log.info("[finaliza] PetController - getPetDoClienteComId");
+		return null;
 	}
 }
