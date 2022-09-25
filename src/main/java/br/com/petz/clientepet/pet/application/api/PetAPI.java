fadc381e57c725bf2437d67d295c5b0dac55bf86
Danilo1dev <danilofoxlog@gmail.com>
2022-09-25 12:03:12 -0300

    implementa contrato API

--- src/main/java/br/com/petz/clientepet/pet/application/api/PetAPI.java
+++ src/main/java/br/com/petz/clientepet/pet/application/api/PetAPI.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.UUID;
 import javax.validation.Valid;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -28,4 +29,7 @@ public interface PetAPI {
 	@ResponseStatus(code = HttpStatus.OK)
 	PetClienteDetalhadoResponse getPetDoClienteComId(@PathVariable UUID idCliente,@PathVariable UUID idPet);
 	
+	@DeleteMapping(value = "/{idPet}")
+	@ResponseStatus(code = HttpStatus.NO_CONTENT)
+	void detelePetDoClienteComId(@PathVariable UUID idCliente, @PathVariable UUID idPet);
 }
