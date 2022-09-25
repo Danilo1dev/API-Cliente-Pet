5fa874e32a45289219dd1121c49777b7114e3110
Danilo1dev <danilofoxlog@gmail.com>
2022-09-25 13:11:09 -0300

    ajuste de espacos

--- src/main/java/br/com/petz/clientepet/cliente/domain/Cliente.java
+++ src/main/java/br/com/petz/clientepet/cliente/domain/Cliente.java
@@ -3,7 +3,6 @@ package br.com.petz.clientepet.cliente.domain;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.UUID;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -12,9 +11,7 @@ import javax.persistence.Id;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-
 import org.hibernate.validator.constraints.br.CPF;
-
 import br.com.petz.clientepet.cliente.application.api.ClienteAlteracaoRequest;
 import br.com.petz.clientepet.cliente.application.api.ClienteRequest;
 import lombok.AccessLevel;
--- src/main/java/br/com/petz/clientepet/handler/RestResponseEntityExceptionHandler.java
+++ src/main/java/br/com/petz/clientepet/handler/RestResponseEntityExceptionHandler.java
@@ -2,7 +2,6 @@ package br.com.petz.clientepet.handler;
 
 import java.util.HashMap;
 import java.util.Map;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
